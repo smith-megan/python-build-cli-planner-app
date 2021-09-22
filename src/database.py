8664d0b02a142b14a7c866381696f65927930216
Megan <smith.megan.design@gmail.com>
2021-09-22 11:19:39 -0600

    removed commented code throwing error

--- src/database.py
+++ src/database.py
@@ -21,10 +21,6 @@ def add_reminder(text, date, ReminderClass):
     if not isinstance(reminder, DeadlinedReminder):
         raise TypeError('Invalid Reminder Class')
 
-
-    # if not issubclass(ReminderClass, DeadlinedReminder):
-    #     raise TypeError('Invalid Reminder Class')
-
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
         writer.writerow(reminder)
