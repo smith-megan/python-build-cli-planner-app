237c1c5f789840064e2a304a20d4705e544d3a6e
Megan <smith.megan.design@gmail.com>
2021-09-22 09:22:54 -0600

    building deadline classes

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,8 +1,17 @@
 from abc import ABCMeta, abstractmethod
 from collections.abc import Iterable
+from abc import ABC
 
 class DeadlinedMetaReminder(Iterable, ABCMeta):
   @abstractmethod
   def is_due(self):
     pass
+  def __iter__(Iterable):
+    pass
 
+class DeadlinedReminder(Iterable, ABC):
+  @abstractmethod
+  def is_due(self):
+    pass
+  def __iter__(Iterable):
+    pass
